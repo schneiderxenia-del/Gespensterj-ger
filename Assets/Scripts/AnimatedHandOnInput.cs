9c46229a97280a7c612ecd145a64410ab40ab74c
Xenia <schneider.xenia@gmail.com>
2025-12-07 14:37:11 +0100

    Hand kann Bogen greifen

--- Assets/Scripts/AnimatedHandOnInput.cs
+++ Assets/Scripts/AnimatedHandOnInput.cs
@@ -1,17 +1,42 @@
 using UnityEngine;
+
 using UnityEngine.InputSystem;
+
 
 
 public class AnimateAndOnInput : MonoBehaviour
 {
+    [Header("Optional: Automatische Anbindung für Bogen-Interaktion")]
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable bowGrabInteractable;
+
     public InputActionProperty triggerValue;
     public InputActionProperty gripValue;
 
     public Animator handAnimator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
+    void Awake()
+    {
+        // Automatische Listener-Anbindung, falls Referenz gesetzt
+        if (bowGrabInteractable != null)
+        {
+            bowGrabInteractable.selectEntered.AddListener(_ => OnBowGrabbed());
+            bowGrabInteractable.selectExited.AddListener(args => {
+                if (!args.isCanceled) OnBowReleased();
+            });
+        }
+    }
+
+    // Beispiel: Diese Methoden können von einem XR-Grab-Event oder Collider-Trigger aufgerufen werden
+    public void OnBowGrabbed()
+    {
+        if (handAnimator != null)
+            handAnimator.SetBool("IsHoldingBow", true);
+    }
+
+    public void OnBowReleased()
+    {
+        if (handAnimator != null)
+            handAnimator.SetBool("IsHoldingBow", false);
     }
 
     // Update is called once per frame
@@ -22,6 +47,10 @@ public class AnimateAndOnInput : MonoBehaviour
 
         handAnimator.SetFloat("Trigger", trigger);
         handAnimator.SetFloat("Grip", grip);
-
+        // Der Parameter "IsHoldingBow" wird über OnBowGrabbed/OnBowReleased gesetzt
     }
+
+    // Beispiel für die Anbindung (z.B. im XR Grab Interactable):
+    // bowGrabInteractable.onSelectEntered.AddListener(handScript.OnBowGrabbed);
+    // bowGrabInteractable.onSelectExited.AddListener(handScript.OnBowReleased);
 }
